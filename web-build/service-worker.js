1624d96a83152fd310fc935c7c9bdf7cfb8411d3
Coala1010 <coalabear1010@gmail.com>
2020-09-28 08:28:14 +0100

    build for slugfield

--- web-build/service-worker.js
+++ web-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4437597de2fc1617177e56c8c31e18a.js"
+  "/precache-manifest.2283684ec597928f843fb5cb7ea537e5.js"
 );
 
 workbox.skipWaiting();
